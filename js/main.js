5e689ac82b07190aa423654c17b373487148299f
Jacob Seiler <seiler.jacob1@gmail.com>
2019-07-30 01:35:15 -0400

    Redesigned Skills section. Added animations.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,44 @@
 const navContent = document.querySelector(".nav-content");
 const navButton = document.querySelector(".nav-collapse");
-const years = document.querySelectorAll(".year");
-const date = new Date();
+
+const skillsCover = document.querySelector(".skills-cover");
+const skillsLogos = document.querySelector(".skills-logos");
+
+const skillsInfoCompany = document.querySelector(".company");
+const skillsInfoDate = document.querySelector(".date");
+const skillsInfoTitle = document.querySelector(".title");
+const skillsInfoDescription = document.querySelector(".desc");
 
 function toggleNavbar() {
 	navButton.classList.toggle("nav-active");
 	navContent.classList.toggle("nav-active");
 }
 
-function formatDates() {
-	for (let i = 0; i < years.length; i++) {
-		let year = years[i];
-		let value = parseInt(year.textContent, 10);
-		let duration = date.getFullYear() - value;
-		let word = duration === 1 ? "year" : "years";
-
-		if (duration <= 0) {
-			duration = "<1";
-			word = "year";
+function toggleSkillsInfo(e, ref) {
+	if (skillsCover.classList.contains("skills-cover-active")) {
+		for (let i = 0; i < skillsLogos.children.length; i++) {
+			skillsLogos.children[i].children[0].classList.remove("skill-logo-active");
 		}
 
-		year.textContent = duration + " " + word;
-		year.setAttribute("tooltip", "Since " + value);
+		skillsCover.classList.remove("skills-cover-active");
+	} else {
+		// Write to DOM info from JSON
+		const xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				const response = JSON.parse(xhttp.responseText)[ref];
+
+				skillsInfoCompany.textContent = response["company"];
+				skillsInfoDate.textContent = response["date"];
+				skillsInfoTitle.textContent = response["title"];
+				skillsInfoDescription.textContent = response["desc"];
+			}
+		};
+		xhttp.open("GET", "js/experience.json", true);
+		xhttp.send();
+
+		skillsCover.classList.add("skills-cover-active");
+		e.classList.add("skill-logo-active");
 	}
 }
 
@@ -30,6 +47,4 @@ document.addEventListener("DOMContentLoaded", () => {
 	navButton.addEventListener("click", () => {
 		toggleNavbar();
 	});
-
-	formatDates();
 });
