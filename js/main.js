86f602ec0d121ea75c02605d3a9ed28f117538ec
Jacob Seiler <seiler.jacob1@gmail.com>
2019-08-20 20:55:03 -0400

    Animated nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navButton = document.querySelector(".nav-button");
+const navContent = document.querySelector(".nav-content");
 
 function toggleNav() {
-    navButton.classList.toggle("nav-active");
+	navButton.classList.toggle("nav-active");
+	navContent.classList.toggle("nav-active");
 }
