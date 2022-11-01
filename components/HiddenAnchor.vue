6f829dc973259a1c1427f76f0562709a377fee5f
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-01 16:59:17 -0400

    Added scrolling on button click

---
+++ components/HiddenAnchor.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="" @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HiddenAnchor'
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container text-center">
+    <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
+    <HiddenAnchor @click="scrollTo('projects')">Skip to projects</HiddenAnchor>
     <div ref="top" class="pb-32" />
-    <!-- image -->
     <Headshot class="mx-auto" />
     <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
     <h1 class="text-green font-bold mt-16 text-5xl tiny:text-4xl">Jacob Seiler</h1>
     <SocialLinks />
+    <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
+    <HiddenAnchor @click="scrollTo('footer')">Skip to footer</HiddenAnchor>
     <p class="mt-40 text-md tiny:text-sm">
       <b>I like to work hard and have fun doing it.</b> As a
       <b>software engineer</b> working for <b>Amazon</b>, and software design
       graduate from <b>Queen's University</b>, I continually look for ways to
       learn and utilize new skills.
     </p>
+    <div ref="projects" />
     <ProjectCards />
     <div class="p-end">
       <p class="text-md tiny:text-sm mb-8">
         Oh, I also like scuba diving and playing guitar.
       </p>
-      <button class="text-green underline cursor-pointer text-md tiny:text-sm" @click="scrollTo('top')">
+      <button ref="footer" class="text-green underline cursor-pointer text-md tiny:text-sm" @click="scrollTo('top')">
         Back to top
       </button>
     </div>
-    <!-- back to top -->
   </div>
 </template>
 
@@ -33,7 +36,8 @@ export default Vue.extend({
   methods: {
     scrollTo (refName: string) {
       const element = this.$refs[refName] as undefined | HTMLElement
-      if (element) { window.scrollTo({ top: element.offsetTop, behavior: 'smooth' }) }
+      if (element === undefined) { return }
+      window.scrollTo({ top: element.offsetTop, behavior: 'smooth' })
     }
   }
 })
