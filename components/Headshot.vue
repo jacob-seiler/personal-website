6b50df1cde690a1f6e8a208901fb90cea61d3ead
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-02 22:59:16 -0400

    Small cleanup

--- components/Headshot.vue
+++ components/Headshot.vue
@@ -1,3 +1,3 @@
 <template>
-    <nuxt-picture class="p-none shadow-light w-headshot tiny:w-headshot-tiny rounded-full" src="headshot.webp" alt="Headshot" />
+    <NuxtPicture class="p-none shadow-light w-headshot tiny:w-headshot-tiny rounded-full" src="headshot.webp" alt="Headshot" />
 </template>
