b4be8d41a4015e02eabc411958893592f0e178e3
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-03 16:57:08 -0400

    Styled hidden links

--- components/HiddenAnchor.vue
+++ components/HiddenAnchor.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="handleClick">
+    <button @click="handleClick" class="hidden-anchor fixed top-0 bg-green text-white shadow-light dark:shadow-heavy p-card font-bold text-xl tiny:text-lg">
         <slot></slot>
     </button>
 </template>
@@ -15,3 +15,15 @@ const handleClick = () => {
     props.el.blur();
 }
 </script>
+
+<style scoped lang="scss">
+.hidden-anchor {
+    z-index: 9999;
+    transform: translate(-50%, -200px);
+	transition: transform 0.3s;
+
+    &:focus {
+        transform: translate(-50%, 20px);
+    }
+}
+</style>
