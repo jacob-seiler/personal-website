465d0719fb8c405b1d179b3b022d4031ee5e13ba
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-02 23:02:31 -0400

    Hover effect added to project card anchor

--- components/ProjectCardAnchor.vue
+++ components/ProjectCardAnchor.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLink :to="href" target="_blank" class="ml-8 tiny:ml-none tiny:mr-8 tiny:my-8">
+    <NuxtLink :to="href" target="_blank" class="ml-8 tiny:ml-none tiny:mr-8 tiny:my-8 hover:opacity-70">
         <NuxtImg :src="icon" :alt="alt" class="text-white w-card h-card tiny:w-card-tiny tiny:h-card-tiny" />
     </NuxtLink>
 </template>
