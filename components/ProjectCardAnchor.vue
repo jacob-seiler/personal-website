1056d5459e2d905155df669e31d99a99e4035da4
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-07 17:27:51 -0500

    Updated screen reader text to match aria label

--- components/ProjectCardAnchor.vue
+++ components/ProjectCardAnchor.vue
@@ -3,7 +3,7 @@
         :to="href"
         target="_blank"
         class="ml-8 tiny:ml-none tiny:mr-8 tiny:my-8 hover:opacity-70"
-        :aria-label="`${alt}: ${name} (opens in a new window)`"
+        :aria-label="ariaLabel"
     >
         <NuxtImg
             class="text-white w-card h-card tiny:w-card-tiny tiny:h-card-tiny"
@@ -13,15 +13,19 @@
             width="tiny:28.5px sm:38px"
             height="tiny:28.5px sm:38px"
         />
-        <span class="sr-only">opens in a new tab</span>
+        <span class="sr-only">{{ ariaLabel }}</span>
     </NuxtLink>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
     href: string;
     icon: string;
     alt: string;
     name: string;
 }>();
+
+const ariaLabel = computed(() => {
+    return `${props.alt}: ${props.name} (opens in a new window)`
+})
 </script>
