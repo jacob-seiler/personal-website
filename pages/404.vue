774a7feffc85d0f96b51cb7200c3b0f3664bde58
Jacob Seiler <seiler.jacob1@gmail.com>
2022-11-07 21:29:10 -0500

    Remove temp cat fact

--- pages/404.vue
+++ pages/404.vue
@@ -2,10 +2,5 @@
     <main class="container mt-32 text-center">
         <h1 class="font-bold text-5xl tiny:text-4xl">Hmm... You seem lost.</h1>
         <p class="mt-16 text-md tiny:text-sm">Not really sure how that happened since there's only one page. But, you can click <NuxtLink to="/" aria-label="right here" class="text-green underline cursor-pointer font-bold">right here</NuxtLink> to get back.</p>
-        <p class="mt-16">{{data.fact}}</p>
     </main>
 </template>
-
-<script setup>
-const { data } = await useFetch('https://catfact.ninja/fact');
-</script>
