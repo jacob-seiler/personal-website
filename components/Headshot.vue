05f438c4b93885523fcfb51bca89ac97b37345b5
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-09 15:28:22 -0500

    Clean slate

--- components/Headshot.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <NuxtImg
-        class="bg-white p-none shadow-light dark:shadow-heavy w-headshot h-headshot tiny:w-headshot-tiny tiny:h-headshot-tiny rounded-full"
-        src="v1667806508/personal%20website/headshot.webp"
-        alt="Headshot"
-        sizes="tiny:412px sm:500px"
-    />
-</template>
