0f75eb366f2523892d4730bf25f4225076072bc4
Jacob Seiler <seiler.jacob1@gmail.com>
2022-12-12 17:22:03 -0500

    Added dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		container: {
@@ -29,5 +30,4 @@ module.exports = {
           tiny: { max: '430px' }
         },
 	},
-	plugins: [],
 }
