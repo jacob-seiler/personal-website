47e7b04dd5219e1b8ed60ca3bb5068cd6a656e07
Jacob Seiler <seiler.jacob1@gmail.com>
2024-09-02 22:47:57 -0400

    Updated to astro v4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,31 @@
 module.exports = {
-  overrides: [
-    {
-      extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
+    overrides: [
+        {
+            extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
+            files: ["*.ts", "*.tsx"],
+            parserOptions: {
+                project: "tsconfig.json",
+            },
+        },
+        {
+            files: ["*.cjs"],
+            rules: {
+                "@typescript-eslint/no-var-requires": 0,
+            },
+            parserOptions: {
+                project: null,
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
         project: "tsconfig.json",
-      },
     },
-    {
-      files: ["*.cjs"],
-      rules: {
-        "@typescript-eslint/no-var-requires": 0,
-      },
-      parserOptions: {
-        project: null,
-      },
+    plugins: ["@typescript-eslint"],
+    extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+    ignorePatterns: ["dist", "node_modules"],
+    rules: {
+        "@typescript-eslint/consistent-type-imports": "warn",
+        "prettier/prettier": "warn",
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-  ignorePatterns: ["dist", "node_modules"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": "warn",
-    "prettier/prettier": "warn",
-  },
 };
